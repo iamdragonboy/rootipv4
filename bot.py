39f04fb7c88e141f6a1f9bc736dd01634ee70bcf
iamdragonboy <ruhaniarif234@gmail.com>
2025-08-20 16:33:21 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ with open("config.json") as f:
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="./", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
